c83a4601f492f57977d67bc87c791bfdbe18332e
Keyuan Wang <wangkeyuan1998@gmail.com>
2022-02-25 19:12:07 +0100

    Scientific computing set 1 all done!

--- Scientific_Computing/Set 1 report/Exercise_1_2.py
+++ Scientific_Computing/Set 1 report/Exercise_1_2.py
@@ -20,41 +20,40 @@ n_k = len(mesh_t)
 c = np.zeros((n_i, n_i))
 c[:, -1].fill(1)
 
+c_next = np.zeros((n_i, n_i))
+c_next[:, -1].fill(1)
 
-def diffusion_solver(i, j, D=1, dx=delta_x, dt=delta_t):
+
+def diffusion_solver(j, D=1, dx=delta_x, dt=delta_t):
+
+    cx = np.zeros(n_i)
 
     # boundary conditions
-    if i == 0:
-        c_ = c[i, j] + dt*D/dx**2 * (c[i+1, j] + c[-(i+1), j] + c[i, j+1] + c[i, j-1] - 4*c[i, j])
-    elif i == n_i-1:
-        c_ = c[i, j] + dt*D/dx**2 * (c[0, j] + c[i-1, j] + c[i, j+1] + c[i, j-1] - 4 * c[i, j])
-    else:
-        c_ = c[i, j] + dt*D/dx**2 * (c[i+1, j] + c[i-1, j] + c[i, j+1] + c[i, j-1] - 4*c[i, j])
-
-    return c_
-
-
-def parallel_solving_pde(j):
-
-    results = []
-    c_new = np.zeros(n_i)
-
-    # Define the number of processors used
-    pool = mp.Pool(mp.cpu_count())
-
     for i in range(n_i):
-        result = pool.apply_async(diffusion_solver, args=(i, j+1))
-        results.append(result)
+        if i == 0:
+            c_ = c[i, j] + dt*D/dx**2 * (c[i+1, j] + c[-(i+1), j] + c[i, j+1] + c[i, j-1] - 4*c[i, j])
+        elif i == n_i-1:
+            c_ = c[i, j] + dt*D/dx**2 * (c[0, j] + c[i-1, j] + c[i, j+1] + c[i, j-1] - 4 * c[i, j])
+        else:
+            c_ = c[i, j] + dt*D/dx**2 * (c[i+1, j] + c[i-1, j] + c[i, j+1] + c[i, j-1] - 4*c[i, j])
+        
+        cx[i] = c_
+    c_next[:, j] = cx
 
-    pool.close()
-    pool.join()
+    return cx
 
-    i = 0
-    for r in results:
-        c_new[i] = r.get()
-        i += 1
 
-    return c_new
+def parallel_solving_pde():
+
+    results = np.zeros((n_i, n_i))
+    results[:, -1].fill(1)
+
+    for j in range(n_i-2):
+        j += 1
+        p = mp.Process(target=diffusion_solver, args=(j, ))
+        p.start()
+
+    return results
 
 
 # Create matrix to store solution at every 1000 time points
@@ -62,17 +61,13 @@ c_store = np.zeros((int(n_k/100), n_i, n_i))
 
 if __name__ == '__main__':
 
-    c_k_1 = c.copy()
-
     for k in range(n_k):
 
         count = 0
-        c = c_k_1
 
-        for j in range(n_i-2):
-            c_next = parallel_solving_pde(j)
-            c_k_1[:, j] = c_next
+        parallel_solving_pde()
+        c = c_next
 
         if k % 100 == 0:
-            c_store[count, :, :] = c_k_1
+            c_store[count, :, :] = c
             print(k, ' time step achieved!')
